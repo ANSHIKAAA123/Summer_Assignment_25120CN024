2b7057c614b2ee0c8f13342c3b5cfaff5fc6fd2c
Anshika Tripathi <25120cn024@glbitm.ac.in>
2026-06-06 19:51:04 +0530

    Update PROGRAM18.C

--- week-1/DAY-5/PROGRAM18.C
+++ week-1/DAY-5/PROGRAM18.C
@@ -2,7 +2,31 @@
 
 int main()
 {
-   int n, tem, rem, sum = 0, fact, i;
+    int n, temp, rem, sum = 0, fact, i;
 
-   printf("Enter a number: ");
-   scanf("")
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(temp > 0)
+    {
+        rem = temp % 10;
+        fact = 1;
+
+        for(i = 1; i <= rem; i++)
+        {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        temp = temp / 10;
+    }
+
+    if(sum == n)
+        printf("%d is a Strong Number.", n);
+    else
+        printf("%d is not a Strong Number.", n);
+
+    return 0;
+}
