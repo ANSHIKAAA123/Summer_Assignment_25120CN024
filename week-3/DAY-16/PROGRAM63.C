72cbcd2aac0c8fe54bd282bc705655b005c73b37
Anshika Tripathi <25120cn024@glbitm.ac.in>
2026-06-19 23:47:27 +0530

    Create PROGRAM63.C

---
+++ week-3/DAY-16/PROGRAM63.C
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, j, target;
+    int found = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter target sum: ");
+    scanf("%d", &target);
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(arr[i] + arr[j] == target)
+            {
+                printf("Pair Found: %d and %d\n", arr[i], arr[j]);
+                found = 1;
+                break;
+            }
+        }
+
+        if(found)
+            break;
+    }
+
+    if(!found)
+    {
+        printf("No pair found.");
+    }
+
+    return 0;
+}
